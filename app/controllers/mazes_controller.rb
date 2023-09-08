bf99177f0b20f3643b7b9d5a9a2b1620925a36f2
Jimmy Jurado <jurado140@gmail.com>
2023-09-08 14:57:52 -0400

    fixed maze generate method.

--- app/controllers/mazes_controller.rb
+++ app/controllers/mazes_controller.rb
@@ -1,18 +1,24 @@
 class MazesController < ApplicationController
 
+  def initialize
+    @height = 10
+    @width = 10  
+    @maze = Array.new(@height) { Array.new(@width, 1) }
+  end
+
   def generate
-    width = 10
-    height = 10
+    start_row, start_col = rand(@height), rand(@width)
+    @maze[start_row][start_col] = 0
 
-    @maze = Array.new(height) { Array.new(width, 1)}
+    visited = Set.new
+    recursive_backtracker(start_row, start_col, visited, @maze)
 
-    recursive_backtracker(rand(height), rand(width))
-
-    render json: { maze: @maze}
+    render json: {maze: @maze}
   end
 
   def solve
     start = [0, 0]
+    
     exit = [@maze.length - 1, @maze[0].length - 1]
 
     visited = []
@@ -23,7 +29,7 @@ class MazesController < ApplicationController
       current = stack.pop
   
       if current == exit
-        break # Exit the loop if the end is reached
+        break 
       end
   
       visited << current
@@ -50,35 +56,46 @@ class MazesController < ApplicationController
   end
 
   private
-  def recursive_backtracker(row, col, visited=[])
+
+  def recursive_backtracker(row, col, visited, maze)
+    return unless valid_movement?(row, col, visited)
+    
+    visited << [row, col]
+    maze[row][col] = 0
+
     directions = [[-1, 0], [0, 1], [1, 0], [0, -1]].shuffle
 
-    visited << [row, col]
-    @maze[row][col] = 0
+    directions.each do |dr, dc|
+        new_row, new_col = row + dr, col + dc
+        next_row, next_col = row + 2*dr, col + 2*dc
+
+        if valid_movement?(next_row, next_col, visited)
+
+            maze[new_row][new_col] = 0
+            
+            recursive_backtracker(next_row, next_col, visited, maze)
+        end
+    end
+  end
+
+  def valid_movement?(row, col, visited)
+    row.between?(0, @maze.length - 1) &&
+      col.between?(0, @maze[0].length - 1) &&
+      !visited.include?([row, col])
+  end
+
+  def get_neighbors(row, col)
+    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    neighbors = []
 
     directions.each do |dr, dc|
       new_row, new_col = row + dr, col + dc
-
-      if new_row.between?(0, @maze.length - 1) && new_col.between?(0, @maze[0].length - 1) && !visited.include?([new_row, new_col])
-
-        @maze[row + dr / 2][col + dc / 2] = 0
-
-        recursive_backtracker(new_row, new_col, visited)
+      if new_row.between?(0, @maze.length - 1) && new_col.between?(0, @maze[0].length - 1)
+        neighbors << [new_row, new_col]
       end
     end
-    
-    def get_neighbors(row, col)
-      directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-      neighbors = []
-    
-      directions.each do |dr, dc|
-        new_row, new_col = row + dr, col + dc
-        if new_row.between?(0, @maze.length - 1) && new_col.between?(0, @maze[0].length - 1)
-          neighbors << [new_row, new_col]
-        end
-      end
-    
-      return neighbors
-    end
+
+    return neighbors
   end
+
 end
